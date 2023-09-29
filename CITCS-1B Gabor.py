d00828acb32cd092db93e5496e667a28a4aeee63
Mark Dunstan <146111861+markdgabor@users.noreply.github.com>
2023-09-29 23:46:38 +0800

    Add files via upload

---
+++ CITCS-1B Gabor.py
@@ -0,0 +1,50 @@
+
+Poundslbs = 220.486
+kilogramsKG = 100
+dividingline = "======================================="
+Miles = 86.3
+Kilometers = 138.9
+TemperatureF = 66
+TemperatureC = 19
+Student1 = 19
+Student2 = 18
+Student3 = 18
+Student4 = 20
+Student5 = 16
+Student6 = 17
+Student7 = 21
+Student8 = 22
+Student9 = 24
+Student10 = 26
+AverageAge = 20.1
+print("Weight in pounds (lbs):",Poundslbs)
+print("Weight converted to kilograms (kg):",kilogramsKG)
+print(dividingline)
+print("Candon to Baguio (mi):", Miles)
+print("Candon to baguio (km):", Kilometers)
+print(dividingline)
+print("Temperator in Baguio (°F):",TemperatureF)
+print("Temperator in Baguio (°C):",TemperatureC)
+print(dividingline)
+print("Age of Student 1:",Student1)
+print("Age of Student 2:",Student2)
+print("Age of Student 3:",Student3)
+print("Age of Student 4:",Student4)
+print("Age of Student 5:",Student5)
+print("Age of Student 6:",Student6)
+print("Age of Student 7:", Student7)
+print("Age of Student 8:",Student8)
+print("Age of Student 9:",Student9)
+print("Age of Student 10:",Student10)
+print("The Student Average age of the students is: ",AverageAge)
+print(dividingline)
+characters = ["Dolphine","GoobaShrimp","Autistic Monkey","Water Melon Warrior","Ninja"]
+Equipment = ["Bonk","Pipe Bomb","Saddle with minigun","Armor","Whip"]
+Ability = ["Mind Control","Majestic Shout","Time leap","Berserk","Concealment"]
+print("In Desperate Figthing",characters[1],"pulls out his",Equipment[1],"and it explodes dealing damage to",characters[2])
+print(characters[0],"responds by using",Equipment[2],"and",Ability[0],"to mow down his men","and then",characters[3],"and",characters[4])
+print("Finally arrived to cut off the enemy line of retreat and instantly put their",Equipment[3],"and",Equipment[4],"To work. Not only that but they came prepared ")
+print("To play their trump cards",Ability[3],"and",Ability[4],". Now at the verge of defeat and visually surrounded",characters[1],"uses his last trick",)
+print("his",Ability[1],"which stuns the entire battlefield",characters[2],"uses the forbidden ability",Ability[2],"to accelerate his body's recovery")
+print("and to propell himself towards GoobaShrimp and finishing him off by using strongest",Equipment[0],"in his entire career however pushing his body")
+print("To the limit too much he slowly succumbs to his wounds and dies gracefully surrounded by his generals")
