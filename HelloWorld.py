2f0377def3924d460908262a1655a8c44183ec31
Mark Dunstan <146111861+markdgabor@users.noreply.github.com>
2023-10-02 21:35:38 +0800

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+print("Hello World!")
+# Put your name here
+name = input("What is your name? - ")
+# Put your age here
+age_input = input("What is your age? - ")
+
+# Convert your age input into an integer
+age = int(age_input)
+
+age_next_year = age + 1
+
+modulo = 5%3
+
+pi = 3.1416
+
+print(f"Hello, {name}", end=" & ")
+print("Hello, "+name)
+print("Hello, my name is "+name, sep="%%%%")
+print(f"My age next year is {age_next_year}")
+print(f"Pi value is {pi}")
+print(f"5 modulo 3 is {5%3}")
